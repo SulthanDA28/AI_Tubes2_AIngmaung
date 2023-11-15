83f2db1664088babe6be0460427b064ddb7007a7
SulthanDA28 <13521159@std.stei.itb.ac.id>
2023-11-15 10:58:27 +0700

    add performance knn

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -21,9 +21,9 @@ def knn(data, k, cari):
     result = pd.DataFrame(columns=['distance', 'price_range'], index=range(data.shape[0]))
     for i in range(data.shape[0]):
         jml = 0
-        for col in ['ram','px_width','battery_power']:
+        for col in ['ram','px_width', 'battery_power','px_height']:
             if col != 'price_range':
-                jml += (data.iloc[i][col] - cari[col]) ** 2
+                jml += (data[col].iloc[i] - cari[col]) ** 2
         result['distance'].iloc[i] = np.sqrt(jml)
         result['price_range'].iloc[i]= data.iloc[i]['price_range']
     result = result.sort_values(by=['distance'])
@@ -36,9 +36,9 @@ def knn(data, k, cari):
 
 truePositive = 0
 trueNegative = 0
-
+sqrtn = int(np.sqrt(data.shape[0])) 
 for i in tqdm(range(validation.shape[0])):
-    if validation["price_range"].iloc[i] == knn(data, 15, validation.iloc[i]):
+    if validation["price_range"].iloc[i] == knn(data, sqrtn, validation.iloc[i]):
         truePositive += 1
     else:
         trueNegative += 1
