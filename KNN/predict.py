07da6e19db113394989b3ce6a01cb7b7f66a2222
Muhammad Habibi Husni <habibihusni18@gmail.com>
2023-11-26 21:35:10 +0700

    feat: improved KNN, KNN renamed to predict.py

--- KNN/predict.py
+++ KNN/predict.py
@@ -2,18 +2,22 @@ import pandas as pd
 import numpy as np
 from tqdm import tqdm
 import sys
+from calc_accuracy import calculate_accuracy
 
-data = pd.read_csv("Data/" + sys.argv[1])
-validation = pd.read_csv("Data/" + sys.argv[2])
+data = pd.read_csv(sys.argv[1])
+validation = pd.read_csv(sys.argv[2])
 
-def knn(data, k, cari, features):
+def knn(data, k, cari, features, weights):
     result = pd.DataFrame(columns=['distance', 'price_range'], index=range(data.shape[0]))
-    eucDist = np.linalg.norm(data[features].values - cari[features].values, axis=1)
+    eucDist = np.sqrt(np.sum(weights * (data[features].values - cari[features].values)**2, axis=1))
     result['distance'] = eucDist
     result['price_range'] = data['price_range']
     result = result.sort_values(by=['distance'])
     result = result.iloc[0:k]
-    return result.groupby('price_range').count()[::-1].idxmax()['distance']
+    score = np.zeros(4)
+    for i in range(k):
+        score[result['price_range'].iloc[i]] += 1 / (result['distance'].iloc[i])
+    return np.argmax(score)
 
 def normalization(data, validation):
     for col in data.columns:
@@ -25,14 +29,17 @@ def normalization(data, validation):
     return data, validation
 
 normalization(data, validation)
-print(data[['ram','px_width', 'battery_power', 'px_height']])
-print(validation[['ram','px_width', 'battery_power', 'px_height']])
+# print(data[['ram','px_width', 'battery_power', 'px_height']])
+# print(validation[['ram','px_width', 'battery_power', 'px_height']])
 sqrtn = int(np.sqrt(data.shape[0]))
 
-k = 30
+k = 17
 result = pd.DataFrame(columns=['id', 'price_range'], index=range(validation.shape[0]))
 for i in tqdm(range(validation.shape[0])):
     result['id'].iloc[i] = i
-    result['price_range'].iloc[i] = knn(data, k, validation.iloc[i], ['ram','px_width', 'battery_power', 'px_height'])
-
+    result['price_range'].iloc[i] = knn(data, k, validation.iloc[i], 
+                                        ['ram','px_width', 'battery_power', 'px_height'], 
+                                        [0.7,0.1,0.1,0.1])
+print("accuracy: ", calculate_accuracy(result, validation))
 result.to_csv("KNN/" + sys.argv[3], index=False)
+print("result saved to KNN/" + sys.argv[3])
